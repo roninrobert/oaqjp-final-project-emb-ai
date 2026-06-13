8a920954755f697040bfeba8e81f95cacd5c28d5
Anonymous <anonymous@example.com>
2026-06-13 14:28:28 -0400

    Task 3a

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyze):
@@ -6,4 +7,37 @@ def emotion_detector(text_to_analyze):
     input_json={ "raw_document": { "text": text_to_analyze } }
 
     response = requests.post(url, json = input_json, headers = headers)
-    return response.text
+
+    formatted_response = json.loads(response.text)
+
+    anger_score = formatted_response['emotionPredictions'][0]['emotion']['anger']
+    disgust_score = formatted_response['emotionPredictions'][0]['emotion']['disgust']
+    fear_score = formatted_response['emotionPredictions'][0]['emotion']['fear']
+    joy_score = formatted_response['emotionPredictions'][0]['emotion']['joy']
+    sadness_score = formatted_response['emotionPredictions'][0]['emotion']['sadness']
+
+    dominant_emotion_score=anger_score
+    dominant_emotion_name = 'anger'
+    if disgust_score > dominant_emotion_score:
+        dominant_emotion_score = disgust_score
+        dominant_emotion_name = 'disgust'
+    if fear_score > dominant_emotion_score:
+        dominant_emotion_score = fear_score
+        dominant_emotion_name = 'fear'
+    if joy_score > dominant_emotion_score:
+        dominant_emotion_score = joy_score
+        dominant_emotion_name = 'joy'
+    if sadness_score > dominant_emotion_score:
+        dominant_emotion_score = sadness_score
+        dominant_emotion_name = 'sadness'
+
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion_name
+        }
+
+    return result
